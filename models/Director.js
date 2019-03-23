a352ad0ca2e09469922f65a4d36cb23aac798b28
Okan Yücel <okan@Okan-MacBook-Pro.local>
2019-03-23 20:53:35 +0300

    validasyon işlemleri

--- models/Director.js
+++ models/Director.js
@@ -2,10 +2,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
-
+    name: {
+        type: String,
+        maxlength: [60, '{PATH} alanı, {MAXLENGTH} den KÜÇÜK olmalıdır.'],
+        minlength: [2, '{PATH} alanı {VALUE}, {MINLENGTH} BÜYÜK olmalıdır.']
+    },
+    surname: {
+        type: String,
+        maxlength: [60, '{PATH} alanı, {MAXLENGTH} den KÜÇÜK olmalıdır.'],
+        minlength: [2, '{PATH} alanı {VALUE}, {MINLENGTH} BÜYÜK olmalıdır.']
+    },
+    bio: {
+        type: String,
+        maxlength: [200, '{PATH} alanı, {MAXLENGTH} den KÜÇÜK olmalıdır.'],
+        minlength: [5, '{PATH} alanı {VALUE}, {MINLENGTH} BÜYÜK olmalıdır.']
+    }
 })
 
 module.exports = mongoose.model('director', DirectorSchema)
--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '{PATH} alanı zorunludur.'],
+        maxlength: [20, '{PATH} alanı, {MAXLENGTH} karakterden KÜÇÜK olmalıdır.'],
+        minlength: [3, '{PATH} alanı {VALUE}, {MINLENGTH} karakterden BÜYÜK olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: [30, '{PATH} alanı, {MAXLENGTH} karakterden KÜÇÜK olmalıdır.'],
+        minlength: [3, '{PATH} alanı {VALUE}, {MINLENGTH} karakterden BÜYÜK olmalıdır.']
+    },
+    country: {
+        type: String,
+        maxlength: [30, '{PATH} alanı, {MAXLENGTH} karakterden KÜÇÜK olmalıdır.'],
+        minlength: [3, '{PATH} alanı {VALUE}, {MINLENGTH} karakterden BÜYÜK olmalıdır.']
+    },
+    year: {
+        type: Number,
+        maxlength: [2040, '{PATH} alanı, {MAXLENGTH} den KÜÇÜK olmalıdır.'],
+        minlength: [1900, '{PATH} alanı {VALUE}, {MINLENGTH} BÜYÜK olmalıdır.']
+    },
+    imdb_score: {
+        type: Number,
+        maxlength: [10, '{PATH} alanı, {MAXLENGTH} den KÜÇÜK olmalıdır.'],
+        minlength: [0, '{PATH} alanı {VALUE}, {MINLENGTH} BÜYÜK olmalıdır.']
+    },
     date: {
         type: Date,
         default: Date.now
