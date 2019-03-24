c478990c9cc10504887f3cbd6e280845f954d9b4
Okan Yücel <okan@Okan-MacBook-Pro.local>
2019-03-24 12:00:47 +0300

    first unit test

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
+const server = require('../app')
+
+chai.use(chaiHttp)
+
+describe('Node server', () => {
+    it('(GET /) anasayfyı döndürür', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err,res) => {
+                res.status.should.be.equal(200);
+                //res.should.have.property('status', 400);
+                done()
+            })
+    })
+})
